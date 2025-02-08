808f1d6b3c7f60c38053c7b3d23fe9cf0631a28e
Sandesh Gorathoki <whimosandesh@gmail.com>
2025-02-08 01:08:01 -0500

    Setup the server and run it through server.js

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config()
+import http from 'http';
+import app from './app.js';
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server is running on the port ${process.env.PORT}`);
+});
